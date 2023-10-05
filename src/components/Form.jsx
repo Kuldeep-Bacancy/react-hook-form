4877b31c497ddf754c92747a619fec69151ac71d
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-10-05 16:39:44 +0530

    Form fileds as array of objects, array and dynamic fields

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, useFieldArray } from 'react-hook-form'
 
 function Form() {
-  const { register, handleSubmit, formState, reset } = useForm({
+  const { register, handleSubmit, formState, reset, control } = useForm({
     defaultValues:{
-      username: 'test user'
+      username: 'test user',
+      email: '',
+      channel: '',
+      social: {
+        twitter: '',
+        facebook: ''
+      },
+      phoneNumbers: ["",""],
+      phNumbers: [{ number: "" }]
     }
   });
   const { errors } = formState
@@ -13,6 +21,11 @@ function Form() {
     console.log("Submited Data is", data);
     reset();
   }
+
+  const { fields, append, remove } = useFieldArray({
+    name: 'phNumbers',
+    control: control
+  })
 
   return (
     <div>
@@ -30,6 +43,39 @@ function Form() {
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel", { required: 'Channel is required!' } )} />
         <p className='error'>{errors.channel?.message}</p>
+        
+        <label htmlFor="twitter">Twitter</label>
+        <input type="text" id="twitter" {...register("social.twitter", { required: 'Twitter is required!' })} />
+        <p className='error'>{errors.social?.twitter?.message}</p>
+
+        <label htmlFor="facebook">Facebook</label>
+        <input type="text" id="facebook" {...register("social.facebook", { required: 'Facebook is required!' })} />
+        <p className='error'>{errors.social?.facebook?.message}</p>
+
+        <label htmlFor="primary-phone">Primary Phone number</label>
+        <input type="text" id="primary-phone" {...register("phoneNumbers.0", { required: 'Primary Phone is required!' })} />
+
+        <label htmlFor="secondary-phone">Secondary Phone number</label>
+        <input type="text" id="secondary-phone" {...register("phoneNumbers.1", { required: 'Secondary Phone is required!' })} />
+
+        <label htmlFor='list-of-phone-numbers'>List of Phone Numbers</label>
+        <div>
+          {
+            fields.map((field, index) => {
+              return(
+                <div key={field.id}>
+                  <input type='text' {...register(`phNumbers.${index}.number`)}/>
+                  {
+                    index > 0 && (
+                      <button type="button" onClick={() => remove(index)}>Remove Phone Number</button>
+                    )
+                  }
+                </div>
+              )
+            })
+          }
+          <button type="button" onClick={() => append({number: ''})}>Add Phone Number</button>
+        </div>
 
         <button>Submit</button>
       </form>
